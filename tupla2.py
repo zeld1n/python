4b5fcc389a62d203f2ed0d6f2d814472d824bcc7
zeld1n <zeldin2105@gmail.com>
2023-11-09 10:57:06 +0100

    Create tupla2.py

---
+++ tupla2.py
@@ -0,0 +1,10 @@
+prenotazioni=[]
+
+def aggiungi_prenotazione():
+    nome=input("Nome: ")
+    data=input("Data di prenotazione: ")
+    numeroPersone=int(input("Inserisci numero di persone: "))
+    numeroTavolo=int(input("Inserisci numero di tavolo: "))
+
+        
+
