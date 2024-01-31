cea27199d8f5c8ac758ecb1921583352fa7b01b4
zeld1n <zeldin2105@gmail.com>
2024-01-31 21:16:25 +0100

    31/01

--- rubrica.py
+++ rubrica.py
@@ -1,28 +1,8 @@
 import pprint
 pp=pprint.PrettyPrinter(indent=4)
 
-contacts={'Espinoza'+' '+'Marco':['3290057330','Bareggio','marcoespinoza@gmail.com']}
-
-def leggiDati():
-  nome=input("Inserisci il nome =")
-  while len(nome)==0:
-    nome=input("Inserisci il nome =")
-  cognome=input("Inserisci il cognome =")
-  while len(nome)==0:
-    cognome=input("Inserisci il cognome =")
-  
-  
-
-
-  
-  return [nome,cognome,telefono,indirizzo,email]
+contacts={'b'+' '+'a':['3290057330','Bareggio','marcoespinoza@gmail.com']}
                                                     
-def leggiChiave(): # зрозуміти навіщо ці 2 функциї |
-  #...da completare
-  #...
-  return [nome,cognome]
-
-
 
 def aggiungi(dict):
   print('Codice della funzione aggiungi')
@@ -60,7 +40,7 @@ def cerca(dict):
    cognome=input("Cognome: ")
    for i in dict.keys():
     if(i==cognome+' '+nome):
-      print(dict[cognome+' '+nome].values()) ## Починити вивод !
+      print(dict[cognome+' '+nome]) ## Починити вивод !
       break
         
     
@@ -69,23 +49,32 @@ def cerca(dict):
 
 def mostra_tutti(dict):
   print(dict)
+  pp.pprint(dict)
 
-def modifica(dict):             # Провірити функцию
+def modifica(dict):          
     nome=input("Nome: ")
     cognome=input("Cognome: ")
-    if(dict.keys()==cognome+nome):
-        print("Dati del contatto :",cognome,nome)
-        print(dict[cognome,nome].values())
-        print("Aggiorna i dati (lascia in bianco i campi che non vuoi modificare): ")
-        telefono=int(input("Telefono (9 cifre): "))
-    while telefono!=9 or not telefono.isnumeric() :
-      telefono=int(input("Telefono (9 cifre): "))
+    for i in dict.keys():
+      if(i==cognome+' '+nome):
+          print("Dati del contatto :",cognome,nome)
+          print(dict[cognome+' '+nome])
+          print("Aggiorna i dati (lascia in bianco i campi che non vuoi modificare): ")
+          telefono=input("Telefono (9 cifre): ")
+    while len(telefono)!=9  or not telefono.isnumeric():
+        if(telefono==""):
+          telefono=dict[cognome+' '+nome][0]
+          break
+        else: 
+          telefono=input("Telefono (9 cifre): ")
     indirizzo=input("Inserisci indirizzo: ")
     mail = input("Email (@ richiesta): ")
-    while "@" not in mail:
-      mail = input("Email (@ richiesta): ")
-      dict[cognome,nome]=[telefono,indirizzo,mail]
-
+    while "@" not in mail or not '':
+       if(mail==""):
+          mail=dict[cognome+' '+nome][2]
+          break
+       else: 
+          mail = input("Email (@ richiesta): ")
+    dict[cognome+' '+nome]=[telefono,indirizzo,mail]
 
 while True:
   print('0) Esci')
@@ -99,7 +88,6 @@ while True:
     break
   elif scelta==1:
     aggiungi(contacts)
-    
   elif scelta==2:
     elimina(contacts)
   elif scelta==3:
